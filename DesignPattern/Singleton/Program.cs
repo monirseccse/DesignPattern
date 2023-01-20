669df52e05333b358850b29a371fdbcd598bfd02
monir <monirantor7@gmail.com>
2023-01-20 11:51:32 +0600

    singleton with thread safety

---
+++ DesignPattern/Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using Singleton;
+
+var numbers = Enumerable.Range(0, 10);
+
+Parallel.ForEach(numbers, i =>
+{
+    var vm = VoteMachine.VoteInstance;
+    vm.Register();
+});
+
+Console.WriteLine(VoteMachine.VoteInstance.TotalVote);
